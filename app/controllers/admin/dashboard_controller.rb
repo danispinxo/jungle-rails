c70615504e8f93ed8bc5667b0a139ed6bc02f01b
danispinxo <genericpronoun@gmail.com>
2022-09-21 15:08:07 +0000

    adding counts and bootstrap to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV["ADMIN_USER_NAME"], :password => ENV["ADMIN_PASSWORD"] 
   
   def show
+    @name = ENV["ADMIN_USER_NAME"]
+    @categories = Category.all
+    @total_products = Product.count
+    @total_inventory = Product.sum(:quantity)
   end
 end
