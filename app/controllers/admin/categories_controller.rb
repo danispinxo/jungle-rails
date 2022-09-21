de14fc1f21eea4bd2cf0172b6add80ed5c27a69c
danispinxo <genericpronoun@gmail.com>
2022-09-21 17:07:24 +0000

    setting up erb pages and listing categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,36 @@
 class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with :name => ENV["ADMIN_USER_NAME"], :password => ENV["ADMIN_PASSWORD"] 
+
+  def index
+    @categories = Category.order(id: :asc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :description,
+      :category_id,
+      :quantity,
+      :image,
+      :price
+    )
+  end
+
 end
