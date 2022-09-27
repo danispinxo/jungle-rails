d596408f258601d89f54cdea3c53a7535d82f6d3
danispinxo <genericpronoun@gmail.com>
2022-09-27 19:16:57 +0000

    functional receipt mailer

--- app/mailers/receipt_mailer.rb
+++ app/mailers/receipt_mailer.rb
@@ -1,2 +1,11 @@
 class ReceiptMailer < ApplicationMailer
+  default from: 'no-reply@jungle.com'
+
+  self.delivery_method = :test
+
+  def receipt(order)
+    @order = order
+    mail(to: @order.email, subject: 'Thank you for your purchase!')
+  end
+
 end
