6e82794f8a7a3a51ff717e42195b7faff90fe981
danispinxo <genericpronoun@gmail.com>
2022-09-27 16:26:38 +0000

    setting up mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,13 @@ class UsersController < ApplicationController
   
   def create
     user = User.new(user_params)
+
     if user.save
+      # Tell the UserMailer to send a welcome email after save
+      UserMailer.with(user: @user).welcome_email.deliver_later
+
+      format.json { render json: @user, status: :created, location: @user }
+
       session[:user_id] = user.id
       redirect_to '/'
     else
