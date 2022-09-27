b84e383a69bf77ce6dc886192a7bdb803b06a71e
danispinxo <genericpronoun@gmail.com>
2022-09-27 12:26:26 +0000

    setting up cypress testing for homepage

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,14 @@
+const context = describe
+
+describe('loads the homepage', () => {
+  context('the homepage is valid and contains the proper content', () => {
+    it('loads the products index page', () => {
+      cy.visit('localhost:3000/')
+      cy.contains('Looking for a way to add some life to your home?')
+    })
+
+    it("There are products on the page", () => {
+      cy.get(".products article").should("be.visible");
+    });
+  })
+})
