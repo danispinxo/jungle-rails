1c672bee255bfbcad9fd025837ce1193203cb1d9
danispinxo <genericpronoun@gmail.com>
2022-09-27 20:17:30 +0000

    trying to add error messages to log in unsuccessful:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,12 @@ class SessionsController < ApplicationController
   
   def create
 
-    if user = User.authenticate_with_credentials(params[:email], params[:password])
-      session[:user_id] = user.id
+    if @user = User.authenticate_with_credentials(params[:email], params[:password])
+      session[:user_id] = @user.id
       redirect_to '/'
       # success logic, log them in using cookies
     else
-      redirect_to '/login'
+      render :new
       # failure, render login form
     end
   end
