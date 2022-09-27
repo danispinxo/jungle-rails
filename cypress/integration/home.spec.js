293015a5067b364aa93e754ddd981f1db5eea858
danispinxo <genericpronoun@gmail.com>
2022-09-27 12:37:59 +0000

    cypress tests for homepage are functional

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -10,5 +10,9 @@ describe('loads the homepage', () => {
     it("There are products on the page", () => {
       cy.get(".products article").should("be.visible");
     });
+
+    it("There are 2 products on the page", () => {
+      cy.get(".products article").should("have.length", 2);
+    });
   })
 })
